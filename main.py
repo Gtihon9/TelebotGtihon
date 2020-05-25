36d76f26db73884692cdcc1e5b477f4d336ade9e
Gtihon9 <gtihon9@gmail.com>
2020-05-25 14:50:35 +0300

    Add def upper() on masterbranch

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def start(message):
     send_mess = f"Hi {message.from_user.first_name} !"
     bot.send_message(message.from_user.id, send_mess)
 
-
+@bot.message_handler(func=lambda message: True)
+def upper(message):
+    bot.reply_to(message, message.text.upper())
 
 bot.polling(none_stop=True)
